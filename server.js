51f0f08e64b54a61da7b793c8901d5272e00a94b
JahongirHamidov <hamidovjahongir444@gmail.com>
2021-01-04 22:58:46 +0500

    finish

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
 
-require('dotenv').config()
 app.use(express.json())
 
 const posts = [
-    {
-        username: 'Kyle',
-        title: 'Post 1'
-    },
-    {
-        username: 'Jim',
-        title: 'Post 2'
-    }
+  {
+    username: 'Kyle',
+    title: 'Post 1'
+  },
+  {
+    username: 'Jim',
+    title: 'Post 2'
+  }
 ]
 
-app.get('/posts',(req,res)=>{
-    res.json(posts)
-})
-app.post('/login',(req,res)=>{
-    //auth user
-    const username = req.body.username
-    console.log(username)
-    const user = {name:username}
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({accessToken:accessToken})
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name))
 })
 
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
 
-const PORT = process.env.PORT || 5000
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
 
-app.listen(PORT, console.log('server ulandi'))
+app.listen(3000)
